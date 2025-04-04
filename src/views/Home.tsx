a28170514f3a34fc981ba1605d6533456b101620
SecondNiceee <col1596321@gmail.com>
2025-04-04 15:51:39 +0300

    dsad

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -9,8 +9,6 @@ export default function Home() {
 
   console.log("Привет")
 
-  const connector = new TonConnect();
-
   const [tonConnectUI] = useTonConnectUI();
 
   const [tonWalletAddress, setTonWalletAddress] = useState<string | null>(null);
